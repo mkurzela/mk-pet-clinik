0d5c38f29cfd4164e3f845cf41987173a9a4046b
Miroslaw Kurzela <miroslaw.kurzela@gmail.com>
2021-05-14 17:34:16 +0200

    Initial Commit

---
+++ src/main/java/com/mk/mkpetclinic/MkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.mk.mkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mk/mkpetclinic/MkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.mkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
