42d694e685f862436da92385a923df4dce7a956f
Miroslaw Kurzela <miroslaw.kurzela@gmail.com>
2021-05-26 13:17:17 +0200

    adding base entity. Closes #10

--- pet-clinic-data/src/main/java/com/mk/mkpetlicnic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mk/mkpetlicnic/model/Pet.java
@@ -2,7 +2,7 @@ package com.mk.mkpetlicnic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/mk/mkpetlicnic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/mk/mkpetlicnic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.mk.mkpetlicnic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
